36299de653786440e79e55c454464d384f1d967e
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-12-01 16:10:51 -0600

    added base pokeemerald decomp files

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,31 @@ import { simpleGit, SimpleGit, CleanOptions } from "simple-git";
 const git: SimpleGit = simpleGit().clean(CleanOptions.FORCE);
 
 async function main() {
-  const branch = await git.branchLocal();
-  console.log(branch);
+  console.log((await git.branchLocal()).current);
+
+  try {
+    await git.checkoutLocalBranch("test");
+    console.log((await git.branchLocal()).current);
+  } catch (err) {
+    if (err) console.error(err);
+  }
 }
+
+async function deleteBranch(branch: string) {
+  const curr = (await git.branchLocal()).current;
+  try {
+    if (curr === branch) {
+      console.log(
+        `Cannot delete branch while checked out in it. Let me move us to "main".`
+      );
+      await git.checkout("main");
+    }
+
+    await git.deleteLocalBranch(branch);
+    console.log(`Local branch "${curr}" deleted`);
+  } catch (err) {
+    if (err) console.error(err);
+  }
+}
+
+main();
