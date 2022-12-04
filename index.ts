646a8fcd134b7e8a84e3c2f515bcf9462bfc559e
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-12-04 11:24:44 -0600

    fixed async function call order

--- index.ts
+++ index.ts
@@ -5,23 +5,21 @@ import { MapPlus } from "./utils/sets-maps";
 
 import { cleanUp, getNproc, makeROM } from "./utils/index";
 
-function main() {
-  const featureMap = MapPlus([["Battle Engine Upgrade", addBattleEngine()]]);
-  return async () => {
-    await cleanUp();
+async function main() {
+  const featureMap = MapPlus([["Battle Engine Upgrade", addBattleEngine]]);
+  await cleanUp();
 
-    const prompt = await Prompt();
+  const prompt = await Prompt();
 
-    console.log(`Creating your custom ROM...`);
-    await addMaster();
-    prompt.addons.forEach(async (addon) => {
-      await featureMap.get(addon);
-    });
+  console.log(`Creating your custom ROM...`);
+  await addMaster();
+  prompt.addons.forEach(async (addon) => {
+    await featureMap.get(addon)();
+  });
 
-    // const nproc = await getNproc(); // get nproc value for compiler
-    // await makeROM(nproc);
-    console.log(`Your ROM is complete!`);
-  };
+  // const nproc = await getNproc(); // get nproc value for compiler
+  // await makeROM(nproc);
+  console.log(`Your ROM is complete!`);
 }
 
 async function test() {
